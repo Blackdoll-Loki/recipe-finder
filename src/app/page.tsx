7e690d060dbef1f5fbf885c08858dfbe5e680401
Blackdoll-Loki <helpmeloki7@gmail.com>
2025-02-22 22:15:13 +0200

    add more cuisine options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   const handleCuisineChange = (e: React.ChangeEvent<HTMLSelectElement>) => setCuisine(e.target.value);
   const handlePrepTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => setPrepTime(e.target.value);
 
-  const isNextButtonEnabled = query || cuisine || prepTime;
+  const isNextButtonEnabled = query && cuisine && prepTime;
 
   const handleNextClick = () => {
     const params: Record<string, string> = {};
@@ -54,9 +54,33 @@ export default function Home() {
               className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
             >
               <option value="">Select Cuisine</option>
-              <option value="italian">Italian</option>
-              <option value="mexican">Mexican</option>
+              <option value="african">African</option>
+              <option value="asian">Asian</option>
+              <option value="american">American</option>
+              <option value="british">British</option>
+              <option value="cajun">Cajun</option>
+              <option value="caribbean">Caribbean</option>
               <option value="chinese">Chinese</option>
+              <option value="eastern European">Eastern European</option>
+              <option value="european">European</option>
+              <option value="french">French</option>
+              <option value="german">German</option>
+              <option value="greek">Greek</option>
+              <option value="indian">Indian</option>
+              <option value="irish">Irish</option>
+              <option value="italian">Italian</option>
+              <option value="japanese">Japanese</option>
+              <option value="jewish">Jewish</option>
+              <option value="korean">Korean</option>
+              <option value="latin american">Latin American</option>
+              <option value="mediterranean">Mediterranean</option>
+              <option value="mexican">Mexican</option>
+              <option value="middle eastern">Middle Eastern</option>
+              <option value="nordic">Nordic</option>
+              <option value="southern">Southern</option>
+              <option value="spanish">Spanish</option>
+              <option value="thai">Thai</option>
+              <option value="vietnamese">Vietnamese</option>
             </select>
             <input
               type="number"
